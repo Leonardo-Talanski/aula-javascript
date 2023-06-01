e246d3da6887f6d663363e0e492eb6b6e221b44d
Leonardo Talanski <126731524+Leonardo-Talanski@users.noreply.github.com>
2023-06-01 14:39:06 +0000

    criação de estrutura faça-enquanto

---
+++ estruturas.js
@@ -0,0 +1,58 @@
+/*
+const condicao1 = true; 
+const condicao2 = true;
+
+// se-senao
+
+if(condicao1 && condicao2) {
+    console.log("Verdadeiro 1");
+
+}
+
+
+else if (false) {
+    console.log("verdadeiro2");
+
+}
+
+else {
+    console.log("Falso");
+
+}
+
+
+
+//escolha-caso
+
+const opcao = 9;
+
+switch(opcao){ // escolha
+    case 1: 
+    console.log("Opcao 1");
+    break;// pare
+
+    case 2: 
+    console.log("Opcao 2");
+    break;//pare
+
+    default:  // caso contrário
+    console.log("Opcao 3");
+    break;//pare
+}
+
+
+// Faça enquanto
+
+let contador = 0;
+while (contador < 10) {
+    console.log(contador++);
+    
+}
+*/
+
+// para 
+
+for (let i=0; i < 5; i++) {
+    console.log(i);
+
+}
